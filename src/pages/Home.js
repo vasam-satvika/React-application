788973d9c155035f02d1ece5058b77fb889f7983
vasam-satvika <118091108+vasam-satvika@users.noreply.github.com>
2023-02-09 01:16:41 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,27 +7,27 @@ class Home extends React.Component{
         return(
             <>
            
-       <section class="background firstSection">
-        <div class="box-main">
-            <div class="firsthalf">
-                <p class="text-big">HOW HAPPY ARE YOU?</p>
-                <p class="text-small">The happiness quotient is a model that makes it easier for people to objectively judge their own happiness on multiple aspects.
+       <section className="background firstSection">
+        <div className="box-main">
+            <div className="firsthalf">
+                <p className="text-big">HOW HAPPY ARE YOU?</p>
+                <p className="text-small">The happiness quotient is a model that makes it easier for people to objectively judge their own happiness on multiple aspects.
                     These aspects are called happiness quotients.</p>
-                <div class="buttons">
-                    <button class="btn"><Link to='/Users' style={{textDecoration:"none",color:"white"}}>Check MQ </Link></button>
+                <div className="buttons">
+                    <button className="btn"><Link to='/Users' style={{textDecoration:"none",color:"white"}}>Check MQ </Link></button>
                     
                 </div>
             </div>
-            <div class="secondHalf">
+            <div className="secondHalf">
                <img src="Assets\img\olive.jpg" alt="happiness" ></img>
             </div>
         </div>
     </section>
     <div id="about">
-    <section class="section">
-       <div class="paras">
-        <p class="sectionTag text-big">WHAT'S MQ? </p>
-        <p class="sectionSubTag text-small">Mental health wellness is a key component in establishing and maintaining a healthy lifestyle.
+    <section className="section">
+       <div className="paras">
+        <p className="sectionTag text-big">WHAT'S MQ? </p>
+        <p className="sectionSubTag text-small">Mental health wellness is a key component in establishing and maintaining a healthy lifestyle.
              Mental health wellness is just as important as physical and medical health, so it is important to learn 
              strategies to maintain good mental health.
 
@@ -36,36 +36,36 @@ class Home extends React.Component{
             like self-acceptance, autonomy, quality of relationships, capacity for personal growth, and overall life satisfaction.
         </p>
     </div>
-        <div class="thumbnail">
-            <img src="Assets\img\mq logo.jpg "alt="happiness" class="imgFluid"></img>
+        <div className="thumbnail">
+            <img src="Assets\img\mq logo.jpg "alt="happiness" className="imgFluid"></img>
         </div>
     </section>
-    <section class="section left">
-        <div class="paras">
-         <p class="sectionTag text-big">Why Mental wellness is important? </p>
-         <p class="sectionSubTag text-small">We live in a country where depression is one of the most underrated and overlooked illness that is often ignored because of the fear of being misunderstood by most of the people. We can’t quash the fact that we all encounter some sort of mental disturbances at some point of time in life when we feel bewildered, confused, deep sorrow and lost. The intensity of these situations is such that we hardly can nullify the importance of mental wellness among children, young groups and adults. Yes, there is no age to be a victim of depression and heart quenching void. There are various ways
+    <section className="section left">
+        <div className="paras">
+         <p className="sectionTag text-big">Why Mental wellness is important? </p>
+         <p className="sectionSubTag text-small">We live in a country where depression is one of the most underrated and overlooked illness that is often ignored because of the fear of being misunderstood by most of the people. We can’t quash the fact that we all encounter some sort of mental disturbances at some point of time in life when we feel bewildered, confused, deep sorrow and lost. The intensity of these situations is such that we hardly can nullify the importance of mental wellness among children, young groups and adults. Yes, there is no age to be a victim of depression and heart quenching void. There are various ways
           to improve your mental wellness depending on the severity, counselling or psychological consultation being one of them.
         </p>
      </div>
-         <div class="thumbnail">
-             <img src="Assets\img\balancingstones.jpg"alt="happiness" class="imgFluid"></img>
+         <div className="thumbnail">
+             <img src="Assets\img\balancingstones.jpg"alt="happiness" className="imgFluid"></img>
          </div>
      </section>
     </div> 
 
     
-     <section class="contact" id="contact">
-         <h2 class="text-center">Contact Us</h2>
-         <div class="form">
-         <input type="text" class="form-input" name="name" placeholder="Enter Your Name"></input>
-         <input type="text" class="form-input"name="phone" placeholder="Enter Your Phone"></input>
-         <input type="text"class="form-input" name="email" placeholder="Enter Your Email"></input>
-         <textarea name="text" class="form-input"id="text"cols="47" rows="10" placeholder="Ellaborate your concern"></textarea>
-         <button class="btn dark">Submit</button>
+     <section className="contact" id="contact">
+         <h2 className="text-center">Contact Us</h2>
+         <div className="form">
+         <input type="text" className="form-input" name="name" placeholder="Enter Your Name"></input>
+         <input type="text" className="form-input"name="phone" placeholder="Enter Your Phone"></input>
+         <input type="text"className="form-input" name="email" placeholder="Enter Your Email"></input>
+         <textarea name="text" className="form-input"id="text"cols="47" rows="10" placeholder="Ellaborate your concern"></textarea>
+         <button className="btn dark">Submit</button>
         </div>
         </section>
-        <footer class="background">
-          <div class="text-footer">
+        <footer className="background">
+          <div className="text-footer">
           Copyright &copy; 2027 mentalwellness-All rights reserved 
         </div> 
         </footer>
