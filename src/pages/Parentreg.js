706dbd1fd02773cb51e92138cde6d3d804314509
vasam-satvika <118091108+vasam-satvika@users.noreply.github.com>
2023-02-09 11:25:26 +0530

    Update Parentreg.js

--- src/pages/Parentreg.js
+++ src/pages/Parentreg.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './styles.css';
 class   Parentreg extends React.Component{
     render(){
         
