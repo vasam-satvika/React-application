8d6676d81a407b7eb3ed4cdd8c7631a612f809f9
vasam-satvika <118091108+vasam-satvika@users.noreply.github.com>
2023-02-09 11:24:12 +0530

    Update Managementreg.js

--- src/pages/Managementreg.js
+++ src/pages/Managementreg.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './styles.css';
 class   Managementreg extends React.Component{
     render(){
         
